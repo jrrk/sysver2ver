3ee7fa49b5e57014f0a71a1f64b24a64050b7d5b
Dr Jonathan Kimmitt <jrrk2@cam.ac.uk>
2018-12-12 12:35:15 +0000

    Add larger interface example

---
+++ example/build2.sh
@@ -0,0 +1,14 @@
+verilator \
+--xml-only -Wno-fatal \
++define+FPGA_TARGET_XILINX \
+../ariane-vcs-regression/ariane/src/debug/dm_pkg.sv \
+../ariane-vcs-regression/ariane/include/riscv_pkg.sv \
+../ariane-vcs-regression/ariane/include/ariane_pkg.sv \
+../ariane-vcs-regression/ariane/src/axi/src/axi_pkg.sv \
+../ariane-vcs-regression/ariane/include/axi_intf.sv \
+../ariane-vcs-regression/ariane/include/std_cache_pkg.sv \
+../ariane-vcs-regression/ariane/src/axi_mem_if/src/axi2mem.sv \
+../ariane-vcs-regression/ariane/src/axi_adapter.sv \
+example/master.sv \
+--top-module master
+./vxmlmain obj_dir/Vmaster.xml
