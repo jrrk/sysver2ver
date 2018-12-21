e12c6128a99e80b612f34d7aa386de9b8cede6ae
Dr Jonathan Kimmitt <jrrk2@cam.ac.uk>
2018-12-21 17:09:25 +0000

    Simplify shr example

--- example/shr.v
+++ example/shr.v
@@ -64,44 +64,13 @@ module shr #(
 	parameter [31:0] STACKADDR = 32'h ffff_ffff
 ) (
 	input 		  clk, resetn,
-	output reg 	  trap,
-
-	output reg 	  mem_valid,
-	output reg 	  mem_instr,
-	input 		  mem_ready,
-
-	output reg [31:0] mem_addr,
-	output reg [31:0] mem_wdata,
-	output reg [ 3:0] mem_wstrb,
-	input [31:0] 	  mem_rdata,
-
-	// Look-Ahead Interface
-	output 		  mem_la_read,
-	output 		  mem_la_write,
-	output [31:0] 	  mem_la_addr,
-	output reg [31:0] mem_la_wdata,
-	output reg [ 3:0] mem_la_wstrb,
-
-	// Pico Co-Processor Interface (PCPI)
-	output reg 	  pcpi_valid,
-	output reg [31:0] pcpi_insn,
-	output [31:0] 	  pcpi_rs1,
-	output [31:0] 	  pcpi_rs2,
-	input 		  pcpi_wr,
-	input [31:0] 	  pcpi_rd,
-	input 		  pcpi_wait,
-	input 		  pcpi_ready,
-
-	// IRQ Interface
-	input [31:0] 	  irq,
-	output reg [31:0] eoi,
 
 	output reg [31:0] alu_add_sub,
 	output reg [31:0] alu_shl, alu_shr,
-	output reg alu_eq, alu_ltu, alu_lts,
-	input [31:0] reg_pc, reg_op1, reg_op2, reg_out,
-	input [4:0] reg_sh,
-   
+	output reg 	  alu_eq, alu_ltu, alu_lts,
+	input [31:0] 	  reg_pc, reg_op1, reg_op2, reg_out,
+	input [4:0] 	  reg_sh,
+        input instr_sub, instr_sra, instr_srai,
 	// Trace Interface
 	output reg 	  trace_valid,
 	output reg [35:0] trace_data
@@ -119,84 +88,6 @@ module shr #(
 	localparam [35:0] TRACE_BRANCH = {4'b 0001, 32'b 0};
 	localparam [35:0] TRACE_ADDR   = {4'b 0010, 32'b 0};
 	localparam [35:0] TRACE_IRQ    = {4'b 1000, 32'b 0};
-
-	reg [63:0] count_cycle, count_instr;
-
-	// Instruction Decoder
-
-	reg instr_lui, instr_auipc, instr_jal, instr_jalr;
-	reg instr_beq, instr_bne, instr_blt, instr_bge, instr_bltu, instr_bgeu;
-	reg instr_lb, instr_lh, instr_lw, instr_lbu, instr_lhu, instr_sb, instr_sh, instr_sw;
-	reg instr_addi, instr_slti, instr_sltiu, instr_xori, instr_ori, instr_andi, instr_slli, instr_srli, instr_srai;
-	reg instr_add, instr_sub, instr_sll, instr_slt, instr_sltu, instr_xor, instr_srl, instr_sra, instr_or, instr_and;
-	reg instr_rdcycle, instr_rdcycleh, instr_rdinstr, instr_rdinstrh, instr_ecall_ebreak;
-	reg instr_getq, instr_setq, instr_retirq, instr_maskirq, instr_waitirq, instr_timer;
-
-	reg [regindex_bits-1:0] decoded_rd, decoded_rs1, decoded_rs2;
-	reg [31:0] decoded_imm, decoded_imm_uj;
-	reg decoder_trigger;
-	reg decoder_trigger_q;
-	reg decoder_pseudo_trigger;
-	reg decoder_pseudo_trigger_q;
-	reg compressed_instr;
-
-	reg is_lui_auipc_jal;
-	reg is_lb_lh_lw_lbu_lhu;
-	reg is_slli_srli_srai;
-	reg is_jalr_addi_slti_sltiu_xori_ori_andi;
-	reg is_sb_sh_sw;
-	reg is_sll_srl_sra;
-	reg is_lui_auipc_jal_jalr_addi_add_sub;
-	reg is_slti_blt_slt;
-	reg is_sltiu_bltu_sltu;
-	reg is_beq_bne_blt_bge_bltu_bgeu;
-	reg is_lbu_lhu_lw;
-	reg is_alu_reg_imm;
-	reg is_alu_reg_reg;
-	reg is_compare;
-
-	wire is_rdcycle_rdcycleh_rdinstr_rdinstrh;
-	assign is_rdcycle_rdcycleh_rdinstr_rdinstrh = |{instr_rdcycle, instr_rdcycleh, instr_rdinstr, instr_rdinstrh};
-
-	reg [63:0] q_ascii_instr;
-	reg [31:0] q_insn_imm;
-	reg [31:0] q_insn_opcode;
-	reg [4:0] q_insn_rs1;
-	reg [4:0] q_insn_rs2;
-	reg [4:0] q_insn_rd;
-	reg dbg_next;
-
-	wire launch_next_insn;
-	reg dbg_valid_insn;
-
-	reg [63:0] cached_ascii_instr;
-	reg [31:0] cached_insn_imm;
-	reg [31:0] cached_insn_opcode;
-	reg [4:0] cached_insn_rs1;
-	reg [4:0] cached_insn_rs2;
-	reg [4:0] cached_insn_rd;
-
-	reg set_mem_do_rinst;
-	reg set_mem_do_rdata;
-	reg set_mem_do_wdata;
-
-	reg latched_store;
-	reg latched_stalu;
-	reg latched_branch;
-	reg latched_compr;
-	reg latched_trace;
-	reg latched_is_lu;
-	reg latched_is_lh;
-	reg latched_is_lb;
-	reg [regindex_bits-1:0] latched_rd;
-
-	reg [31:0] current_pc;
-
-	reg [3:0] pcpi_timeout_counter;
-	reg pcpi_timeout;
-
-	reg [31:0] next_irq_pending;
-	reg do_waitirq;
 
 	reg [31:0] alu_out, alu_out_q;
 	reg alu_out_0, alu_out_0_q;
