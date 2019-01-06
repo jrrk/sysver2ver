b25b3319156de9bd64d12b2034d7866bb504bb9e
Dr Jonathan Kimmitt <jrrk2@cam.ac.uk>
2019-01-06 07:11:46 +0000

    Add simpler interface example

---
+++ example/interface1.v
@@ -0,0 +1,35 @@
+// Cut down system verilog test
+
+module interface1 (input clk, input rst, output [31:0] test1);
+
+   // interface instance
+   simple simon0(), simon1[1:0]();
+
+   submodule x1(.conn(simon0));
+   
+   assign test1 = simon1[0].simple1 ^ simon1[1].simple2;
+   
+endmodule
+
+module submodule (simple.dir1 conn);
+
+   assign conn.simple1 = 32'h55AA;
+
+endmodule // submodule
+
+interface simple #(parameter int unsigned WIDTH = 32)();
+
+   // modport signals
+   logic [WIDTH-1:0] simple1, simple2;
+
+   modport dir1 (
+      output simple1,
+      input  simple2
+   );
+
+   modport dir2 (
+      input  simple1,
+      output simple2
+   );
+
+endinterface
